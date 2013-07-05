0cd437d8b683c752b32a30e9837abcfd20a08c90
Scott Vitale <svvitale@gmail.com>
2013-07-05 11:47:53 -0600

    Minor changes to runLook invocation.

--- hipchat-svn-post-commit.py
+++ hipchat-svn-post-commit.py
@@ -28,7 +28,7 @@ ROOM="<room name>"
 NAME="Subversion"
 
 # svnlook location
-LOOK="/usr/bin/svnlook"
+LOOK="svnlook"
 
 ##############################################################
 ##############################################################
@@ -55,11 +55,9 @@ def sendToHipChat( msg, token, room, name ):
 	# urlencode and post
 	urllib2.urlopen( "https://api.hipchat.com/v1/rooms/message", urllib.urlencode( request ) )
   
-def runLook( subcmd, arg1, arg2, arg3 ):
+def runLook( *args ):
 	# check_output will except if it fails so we don't spam the room with 'run svnlook help for help' messages
-	return subprocess.check_output(LOOK + ' ' + subcmd + ' ' + arg1 + ' ' + arg2 + ' ' + arg3, shell=True, stderr=subprocess.STDOUT)
-	#This is from the old subprocess.Popen method
-	#return p.communicate()[0].strip()
+	return subprocess.check_output( ' '.join([LOOK] + list(args)), shell=True, stderr=subprocess.STDOUT)
 
 def getCommitInfo( repo, revision ):
 	comment = runLook("log", repo, "-r", revision)
